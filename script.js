96625804437b2131908a3a4f32d6ac933c77be6d
Denis Dupont <denis.dupont@univ-evry.fr>
2022-01-20 10:03:16 +0100

    grid

---
+++ script.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+ const switcher = document.querySelector('#responsive_headline');
+
+ switcher.addEventListener('click', function() {
+     
+    document.querySelector('.gridcontainer').classList.toggle('dg');
+ 
+ });
